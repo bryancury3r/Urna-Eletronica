15c714dcd8a79a00ba0b9a3a4c8a113b9dabe0d2
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-01 17:29:43 -0300

    add upgrade

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Arhi',
+        partido: 'ABC',
+        fotos: [
+          {url:'./image/1010.jpeg', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: 77222,
+        nome: 'Ashe',
+        partido: 'DEFG',
+        fotos: [
+          {url:'./image/1011.jpeg', legenda: 'Vereador', small: false}
+        ]
+      },
+    ] 
+  },
+
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'ciclano',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos: [
+          {url:'./image/1012.jpeg', legenda: 'Prefeiro'},
+          {url:'./image/1013.jpeg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: 84,
+        nome: 'Ashe',
+        partido: 'DEFG',
+        fotos: [
+          {url:'./image/1010.jpeg', legenda: 'Prefeiro'},
+          {url:'./image/1014.jpeg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ] 
+  },
+];
