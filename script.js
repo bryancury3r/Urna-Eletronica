f428ce7367cd36e894d74c0a9cbeaf1425f3ecc5
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-01 19:31:36 -0300

    script

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ let descricao = document.querySelector('.divisao-1-4');
 let aviso = document.querySelector('.divisao-2');
 let lateral = document.querySelector('.divisao-1-right');
 let numeros = document.querySelector('.divisao-1-3');
+
+function clicou(n) {
+  alert("Clicou em " + n)
+}
+
+function branco() {
+  alert("clicou em branco")
+}
+
+function corrige() {
+  alert("clicou em corrige")
+}
+
+function confirma() {
+  alert("clicou em confirma")
+}
