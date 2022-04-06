cd3886458c0b53a332776cbc9822646d316c4b0e
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-05 22:31:19 -0300

    add functions

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.divisao-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = true;
+let votos = [];
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
@@ -113,18 +114,26 @@ function confirma() {
 
   if(votoBranco === true) {
     votoConfirmado = true;
-    console.log("Confirmando como branco");
+    votos.push({
+        etapa: etapas[etapaAtual].titulo,
+        voto: 'branco'
+    });
+    
   } else if(numero.length === etapa.numeros) {
     votoConfirmado = true;
-    console.log("Confirmando como" + numero);
-  }
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: numero
 
+    });
+  }
   if (votoConfirmado) {
     etapaAtual++;
     if(etapas[etapaAtual] !== undefined) {
       comecarEtapa();
     } else {
-      console.log("FIM!");
+      document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+      console.log(votos)
     }
   }
 }
